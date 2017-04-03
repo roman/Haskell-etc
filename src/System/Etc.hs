cc28d51112e77f218a99706bc32b2e449066fab2
Roman Gonzalez <romanandreg@gmail.com>
2017-04-03 07:45:33 +0000

    Start with documentation

--- src/System/Etc.hs
+++ src/System/Etc.hs
@@ -1,39 +1,61 @@
 {-# LANGUAGE CPP               #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-module System.Etc (
-    module Types
 
+{-|
+
+-}
+
+module System.Etc (
+  -- * Config
+  -- $config
+    Config
+  , getConfigValue
+  , getConfigValueWith
+  , getSelectedConfigSource
+  , getAllConfigSources
+
+  -- * ConfigSpec
+  -- $config_spec
+  , ConfigSource (..)
+  , ConfigValue
+  , ConfigSpec
+  , ConfigurationError (..)
+  , parseConfigSpec
+  , readConfigSpec
+
+  -- ** Resolvers
+  -- $resolvers
   , resolveDefault
   , resolveFiles
   , resolveEnvPure
   , resolveEnv
 
 #ifdef WITH_CLI
-  , getErrorMessage
-  , CliConfigError(..)
+
   , resolvePlainCliPure
   , resolveCommandCliPure
   , resolvePlainCli
   , resolveCommandCli
+
+  -- ** CLI Resolver Error type
+  , getErrorMessage
+  , CliConfigError(..)
 #endif
 
 #ifdef WITH_PRINTER
+  -- * Printer
+  -- $printer
   , renderConfig
   , printPrettyConfig
   , hPrintPrettyConfig
 #endif
-
-  , getConfigValue
-  , getConfigValueWith
-  , getSelectedConfigSource
-  , getAllConfigSources
   ) where
 
 import System.Etc.Internal.Resolver.Default (resolveDefault)
-import System.Etc.Internal.Types            as Types
+import System.Etc.Internal.Types
     (Config, ConfigSource (..), ConfigValue)
-import System.Etc.Spec                      as Types
+import System.Etc.Spec
     (ConfigSpec, ConfigurationError (..), parseConfigSpec, readConfigSpec)
 
 #ifdef WITH_CLI
@@ -50,3 +72,30 @@ import System.Etc.Internal.Config
     (getAllConfigSources, getConfigValue, getConfigValueWith, getSelectedConfigSource)
 import System.Etc.Internal.Resolver.Env  (resolveEnv, resolveEnvPure)
 import System.Etc.Internal.Resolver.File (resolveFiles)
+
+{- $config
+
+   Use this functions to fetch values from the Etc.Config and cast them to types
+   that make sense in your program
+-}
+
+{- $config_spec
+
+   Use this functions to read the configuration spec. Remember you can
+   use JSON or YAML(*) filepaths
+
+   * The yaml cabal flag must be used to support yaml syntax
+-}
+
+{- $resolvers
+
+   Use this functions to gather configuration values from different sources
+   (environment variables, command lines or files). Then compose results
+   together using the mappend function
+-}
+
+{- $printer
+
+   Use these function to render the configuration map and understand how the
+   resolving was performed.
+-}
