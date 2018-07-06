13a76765288b97d5338d2c75c45f41d880135b67
Roman Gonzalez <romanandreg@gmail.com>
2018-07-06 15:45:36 -0700

    [47] Make the parseConfigSpecTH example support multi-command

--- examples/etc-compile-time-spec-check-example/src/Main.hs
+++ examples/etc-compile-time-spec-check-example/src/Main.hs
@@ -1,28 +1,39 @@
+{-# LANGUAGE DeriveLift          #-}
 {-# LANGUAGE NoImplicitPrelude   #-}
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell     #-}
 module Main where
 
-import RIO
+import           RIO
+import qualified Prelude
 
-import Data.Proxy (Proxy)
+import           Data.Proxy (Proxy)
 
 import qualified System.Etc as Etc
 
+-- NOTE: Given TemplateHaskell restrictions, this need to be created
+-- in a different module
+import Types (Cmd(..))
+
 main :: IO ()
 main = do
-  let configSpec = $(Etc.readConfigSpecTH (undefined :: Proxy ()) "./resources/spec.yaml")
+  let configSpec = $(Etc.readConfigSpecTH (undefined :: Proxy Cmd) "./resources/spec.yaml")
 
   Etc.reportEnvMisspellingWarnings configSpec
 
   (configFiles, _fileWarnings) <- Etc.resolveFiles configSpec
   configEnv                    <- Etc.resolveEnv configSpec
-  configOptParser              <- Etc.resolvePlainCli configSpec
+  (cmd, configOptParser)       <- Etc.resolveCommandCli configSpec
 
   let configDefault = Etc.resolveDefault configSpec
 
       config =
         configFiles `mappend` configDefault `mappend` configEnv `mappend` configOptParser
 
-  Etc.printPrettyConfig config
+  case cmd of
+    PrintConfig -> liftIO $ Etc.printPrettyConfig config
+
+    RunMain     -> do
+      Prelude.putStrLn "Executing main program"
+      liftIO $ Etc.printPrettyConfig config
