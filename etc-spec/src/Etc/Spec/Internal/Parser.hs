379f2115036e6ec792cd711a893c09ee45054c7e
Roman Gonzalez <romanandreg@gmail.com>
2018-07-23 08:18:43 -0700

    [no-issue] Start with etc-spec package (pending tests)

---
+++ etc-spec/src/Etc/Spec/Internal/Parser.hs
@@ -0,0 +1,152 @@
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE NamedFieldPuns    #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+module Etc.Spec.Internal.Parser where
+
+import RIO
+import qualified RIO.Map as Map
+import qualified RIO.HashMap as HashMap
+import qualified RIO.Vector.Partial as Vector (head)
+
+import qualified Data.Aeson as JSON hiding (withText)
+import qualified Data.Aeson.BetterErrors as JSON
+
+import Etc.Spec.Internal.Types
+
+--------------------------------------------------------------------------------
+
+matchesConfigValueType :: ConfigValueType -> JSON.Value -> Bool
+matchesConfigValueType cvType json = case (json, cvType) of
+  (JSON.Null    , CVTSingle _        ) -> True
+  (JSON.String{}, CVTSingle CVTString) -> True
+  (JSON.Number{}, CVTSingle CVTNumber) -> True
+  (JSON.Bool{}  , CVTSingle CVTBool  ) -> True
+  (JSON.Object{}, CVTSingle CVTObject) -> True
+  (JSON.Array arr, CVTArray inner) ->
+    if null arr then True else all (matchesConfigValueType (CVTSingle inner)) arr
+  _ -> False
+
+assertFieldTypeMatches ::
+     Monad m
+  => [Text]
+  -> ConfigValueType
+  -> JSON.Value
+  -> JSON.ParseT SpecParserError m ()
+assertFieldTypeMatches fieldKeypath cvType json
+  | matchesConfigValueType cvType json = return ()
+  -- TODO: Create Custom Type
+  | otherwise = JSON.throwCustomError (ConfigValueDefaultTypeMismatchFound fieldKeypath cvType json)
+
+inferConfigValueTypeFromJSON ::
+     Monad m => [Text] -> JSON.Value -> JSON.ParseT SpecParserError m ConfigValueType
+inferConfigValueTypeFromJSON fieldKeypath defaultJSON =
+    either JSON.throwCustomError return (inferJSON defaultJSON)
+  where
+    inferJSON json =
+      case json of
+      JSON.String{} -> Right $ CVTSingle CVTString
+      JSON.Number{} -> Right $ CVTSingle CVTNumber
+      JSON.Bool{}   -> Right $ CVTSingle CVTBool
+      JSON.Array arr
+        -- TODO: Create Custom Type
+        | null arr -> Left (CannotInferTypeFromDefault fieldKeypath json)
+        | otherwise -> case inferJSON (Vector.head arr) of
+          Right CVTArray{}     -> Left (InferredNestedArrayOnDefault fieldKeypath json)
+          Right (CVTSingle ty) -> Right $ CVTArray ty
+          Left  err            -> Left err
+      -- TODO: Create Custom Type
+      _ -> Left (CannotInferTypeFromDefault fieldKeypath json)
+
+parseConfigValueType1 ::
+     (Monad m) => [Text] -> JSON.ParseT SpecParserError m ConfigValueType
+parseConfigValueType1 fieldKeypath = do
+  JSON.withText $ \typeText ->
+    case typeText of
+      "string"   -> Right $ CVTSingle CVTString
+      "number"   -> Right $ CVTSingle CVTNumber
+      "bool"     -> Right $ CVTSingle CVTBool
+      "[string]" -> Right $ CVTArray CVTString
+      "[number]" -> Right $ CVTArray CVTNumber
+      "[bool]"   -> Right $ CVTArray CVTBool
+      "[object]" -> Right $ CVTArray CVTObject
+      -- TODO: Create Custom Type with friendly message
+      -- TODO: Implement tests
+      _ -> Left (InvalidConfigValueType fieldKeypath typeText)
+
+parseConfigValueType ::
+     Monad m
+  => [Text]
+  -> Maybe JSON.Value
+  -> JSON.ParseT SpecParserError m ConfigValueType
+parseConfigValueType fieldKeypath mdefaultValue =
+  case mdefaultValue of
+    Nothing ->
+      JSON.key "type" (parseConfigValueType1 fieldKeypath)
+    Just defaultValue -> do
+      mFieldType <- JSON.keyMay "type" (parseConfigValueType1 fieldKeypath)
+      case mFieldType of
+        Nothing ->
+          inferConfigValueTypeFromJSON fieldKeypath defaultValue
+        Just fieldType -> do
+          assertFieldTypeMatches fieldKeypath fieldType defaultValue
+          return fieldType
+
+parseConfigValueData ::
+     Monad m => [Text] -> JSON.ParseT SpecParserError m ConfigValueData
+parseConfigValueData fieldKeypath = JSON.key "etc/spec" $ do
+  configValueDefault <- JSON.keyMay "default" JSON.asValue
+  -- TODO: make tests around type inference
+  configValueType <- parseConfigValueType fieldKeypath configValueDefault
+  configValueSensitive <- JSON.keyOrDefault "sensitive" False JSON.asBool
+  configValueJSON <- JSON.asValue
+  return
+    $ ConfigValueData
+    {
+      configValueDefault
+    , configValueType
+    , configValueSensitive
+    , configValueJSON
+    }
+
+parseConfigSpecEntries :: Monad m => [Text] -> JSON.ParseT SpecParserError m ConfigValue
+parseConfigSpecEntries fieldKeypath = do
+  jsonValue <- JSON.asValue
+  case jsonValue of
+    JSON.Object object ->
+      case HashMap.lookup "etc/spec" object of
+        Nothing -> parseSubConfig
+        Just _
+          -- TODO: Test this case
+          -- There is more than one entry in the map containing 'etc/spec', this is likely
+          -- an error and should be reported as soon as possible
+          | HashMap.size object > 1 ->
+            JSON.throwCustomError
+              (RedundantKeysOnConfigValue fieldKeypath $
+               HashMap.keys $ HashMap.delete "etc/spec" object)
+          | otherwise -> parseConfigValue
+    _ -> do
+      configValueType <- inferConfigValueTypeFromJSON fieldKeypath jsonValue
+      return $
+        ConfigValue $
+        ConfigValueData
+          { configValueDefault = Just jsonValue
+          , configValueType
+          , configValueSensitive = False
+          , configValueJSON = jsonValue
+          }
+  where
+    parseSubConfig =
+      (SubConfig . Map.fromList) <$>
+      JSON.forEachInObject
+        (\key -> (,) key <$> parseConfigSpecEntries (key : fieldKeypath))
+    parseConfigValue = ConfigValue <$> parseConfigValueData fieldKeypath
+
+parseConfigSpec :: Monad m => JSON.ParseT SpecParserError m ConfigSpec
+parseConfigSpec = do
+  result <- JSON.key "etc/entries" (parseConfigSpecEntries [])
+  configSpecJSON <- HashMap.delete "etc/entries" <$> JSON.asObject
+  case result of
+    ConfigValue {} -> JSON.throwCustomError (InvalidSpecEntries result)
+    SubConfig configSpecEntries ->
+      return $ ConfigSpec {configSpecJSON, configSpecEntries}
---
+++ etc-spec/src/Etc/Spec/Internal/Types.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE DeriveLift #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE NamedFieldPuns    #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+module Etc.Spec.Internal.Types where
+
+import RIO
+
+-- import Language.Haskell.TH.Syntax (Lift(..))
+
+import qualified Data.Aeson as JSON
+
+data SpecParserError
+  = CannotInferTypeFromDefault ![Text] !JSON.Value
+  | InferredNestedArrayOnDefault ![Text] !JSON.Value
+  | InvalidConfigValueType ![Text] !Text
+  | ConfigValueDefaultTypeMismatchFound ![Text] !ConfigValueType !JSON.Value
+  | RedundantKeysOnConfigValue ![Text] ![Text]
+  | InvalidSpecEntries !ConfigValue
+  deriving (Show)
+
+instance Exception SpecParserError
+
+-- |
+data SingleConfigValueType
+  = CVTString
+  | CVTNumber
+  | CVTBool
+  | CVTObject
+  | CVTCustom Text
+  deriving (Generic, Show, Read, Eq)
+
+-- |
+data ConfigValueType
+  = CVTSingle !SingleConfigValueType
+  | CVTArray  !SingleConfigValueType
+  deriving (Generic, Show, Read, Eq)
+
+-- |
+data ConfigValueData =
+  ConfigValueData {
+    configValueDefault    :: !(Maybe JSON.Value)
+  , configValueType       :: !ConfigValueType
+  , configValueSensitive  :: !Bool
+  , configValueJSON       :: !JSON.Value
+  }
+  deriving (Generic, Show, Eq)
+
+-- |
+data ConfigValue
+  = ConfigValue !ConfigValueData
+  | SubConfig   !(Map Text ConfigValue)
+  deriving (Generic, Show, Eq)
+
+-- |
+data ConfigSpec
+  = ConfigSpec { configSpecJSON    :: !JSON.Object
+               , configSpecEntries :: !(Map Text ConfigValue)
+               }
+  deriving (Generic, Show, Eq)
